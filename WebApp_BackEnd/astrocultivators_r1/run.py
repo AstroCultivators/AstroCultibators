fbac0240a8fb1bee283fc263c8a8b0d969ff2d50
coding-yost <77023930+coding-yost@users.noreply.github.com>
2024-04-09 11:57:13 -0700

    changed app to run on server

--- WebApp_BackEnd/astrocultivators_r1/run.py
+++ WebApp_BackEnd/astrocultivators_r1/run.py
@@ -2,5 +2,5 @@ from astrocultivators import app
 
 if __name__ == '__main__':
     host = '130.166.24.117'
-    # app.run(host=host, port=5000, debug=True)
-    app.run(debug=True) # run local
+    app.run(host=host, port=5000, debug=True)
+    # app.run(debug=True) # run local
