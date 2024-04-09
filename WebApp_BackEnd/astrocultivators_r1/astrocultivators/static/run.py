9503ca65c29df232a724da7a733bd6c2fd4ad861
SandraLDavis <sandraldavisnow@gmail.com>
2024-04-09 10:14:07 -0700

    Version 2 running on server

--- WebApp_BackEnd/astrocultivators_r1/astrocultivators/static/run.py
+++ WebApp_BackEnd/astrocultivators_r1/astrocultivators/static/run.py
@@ -1,6 +1,6 @@
 from astrocultivators import app
 
 if __name__ == '__main__':
-    host = '130.166.24.225'
+    host = '130.166.24.117'
     app.run(host=host, port=5000, debug=True)
     # app.run(debug=True) # run local
