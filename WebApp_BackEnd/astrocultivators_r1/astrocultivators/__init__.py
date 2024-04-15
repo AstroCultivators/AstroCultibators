346680489f984ea899517f62718944d453ccfd02
coding-yost <77023930+coding-yost@users.noreply.github.com>
2024-04-15 15:42:11 -0700

    Reinstalled pip and updated, added git ignore file, added Socketio, and got the app running using socket

--- WebApp_BackEnd/astrocultivators_r1/astrocultivators/__init__.py
+++ WebApp_BackEnd/astrocultivators_r1/astrocultivators/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cb8830318b22945dc6f811d2812ef6be' # for use with cookies
+socket = SocketIO(app)
 
 from astrocultivators import routes # placed at end of file to avoid import loops
