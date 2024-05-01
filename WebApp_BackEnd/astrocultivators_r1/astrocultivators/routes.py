50530a571632d1315fac8683c8d3507eabb5b85c
SandraLDavis <sandraldavisnow@gmail.com>
2024-05-01 14:43:29 -0700

    Live sensor data updating

--- WebApp_BackEnd/astrocultivators_r1/astrocultivators/routes.py
+++ WebApp_BackEnd/astrocultivators_r1/astrocultivators/routes.py
@@ -6,6 +6,13 @@ from astrocultivators import app, socket
 import base64 
 from io import BytesIO
 from sensor_figure import get_figure, get_current_data, get_time_data
+from sensor_data import SensorData
+
+port = 1
+address = 0x77
+document_name = 'test'
+
+sensor = SensorData(port, address, document_name)
 
 @socket.on('connect')
 def handle_init_chart():
@@ -15,9 +22,9 @@ def handle_init_chart():
 # Update emitted from liveChart.js
 @socket.on('update')
 def handle_update():
-    pass
+    sensor.run()
     # emit('update-chart', {'currentData': get_current_data() }, broadcast=True)
-    # emit('update-current', get_current_data(), broadcast=True)
+    emit('update-current', get_current_data(), broadcast=True)
 
 @app.route("/")
 @app.route("/home")
--- WebApp_BackEnd/astrocultivators_r1/detect_distance.py
+++ WebApp_BackEnd/astrocultivators_r1/detect_distance.py
@@ -9,12 +9,12 @@ while True:
     ret, depth_frame, color_frame = dc.get_frame()
 
     #Show distance for a specific point
-    point= (400, 300)
-    cv2.circle(color_frame, point, 4, (0, 0, 255))
-    distance = depth_frame[point[1], point[0]]
-    print(distance)
+    point= (400, 300) 
+    cv2.circle(color_frame, point, 4, (0, 0, 255)) 
+    distance = depth_frame[point[1], point[0]]    
+    print(distance) 
 
-    cv2.imshow("depth frame", depth_frame)
+    cv2.imshow("depth frame", depth_frame) 
     cv2.imshow("Color frame", color_frame)
     cv2.waitKey(0)
     key = cv2.waitKey(1)
