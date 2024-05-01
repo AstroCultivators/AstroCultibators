4db7ec36a9945ca6bb0812b148d83415c3f3d156
SandraLDavis <sandraldavisnow@gmail.com>
2024-05-01 15:56:28 -0700

    Distance Camera saves to files

--- WebApp_BackEnd/astrocultivators_r1/detect_distance.py
+++ WebApp_BackEnd/astrocultivators_r1/detect_distance.py
@@ -1,22 +1,41 @@
 import cv2
 import pyrealsense2
 from realsense_depth import *
+import os
+import datetime
 
 #Initialize Camera Intel RealSense
-dc = DepthCamera()
+intelImages = "astrocultivators/static/images/rgb"
+if not os.path.exists(intelImages):
+    os.makedirs(intelImages)
 
-while True:
-    ret, depth_frame, color_frame = dc.get_frame()
+dc = DepthCamera() #initialize camera
+count = 0
+
+while count == 0 :
+    ret, depth_frame, rgb_frame = dc.get_frame()
+    #rgb_frame,depth_frame = dc.get_frame()
+    rgb_filename=os.path.join(intelImages,f"rgb_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.png")
+    cv2.imwrite(rgb_filename,rgb_frame)
+
+    #save depth image with timestamp
+    depth_filename=os.path.join(intelImages,f"depth_{datetime.datetime.now().strftime('%Y%m%d%H%M%S')}.png")
+    cv2.imwrite(depth_filename, depth_frame)
+
+    print("Images saved successfully")
 
     #Show distance for a specific point
     point= (400, 300) 
-    cv2.circle(color_frame, point, 4, (0, 0, 255)) 
+    cv2.circle(rgb_frame, point, 4, (0, 0, 255)) 
     distance = depth_frame[point[1], point[0]]    
     print(distance) 
 
     cv2.imshow("depth frame", depth_frame) 
-    cv2.imshow("Color frame", color_frame)
-    cv2.waitKey(0)
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+    cv2.imshow("Color frame", rgb_frame)
+    count = 1
+
+
+
+#cv2.waitKey(0)
+#key = cv2.waitKey(1)
+
